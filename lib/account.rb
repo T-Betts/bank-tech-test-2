99e44a5692ad31fc3d21480ebe5549c222434b65
Tom Betts <tom.betts@live.co.uk>
2018-04-24 18:43:45 +0100

    Add show statemnt method to satisfy test

--- lib/account.rb
+++ lib/account.rb
@@ -6,12 +6,25 @@ class Account
     @account_history = []
   end
 
-  def deposit(amount)
+  def deposit(amount, transaction = Transaction)
     @balance += amount
+    @account_history << [transaction.new('deposit', amount), @balance]
   end
 
-  def withdraw(amount)
+  def withdraw(amount, transaction = Transaction)
     @balance -= amount
+    @account_history << [transaction.new('withdrawal', amount), @balance]
+  end
+
+  def show_statement
+    puts 'date || credit || debit || balance'
+    @account_history.each do |transaction|
+      if transaction[0].type == 'deposit'
+        puts "#{transaction[0].date} || #{transaction[0].amount} || - || #{transaction[1]}"
+      else
+        puts "#{transaction[0].date} || - || #{transaction[0].amount} || #{transaction[1]}"
+      end
+    end
   end
 
   attr_reader :balance, :account_history
