293c027c9c39edb3d74033cdc61c9dadb894bd55
Tom Betts <tom.betts@live.co.uk>
2018-04-24 16:43:28 +0100

    Write test for current_date method

---
+++ spec/datestamp_spec.rb
@@ -0,0 +1,7 @@
+describe DateStamp do
+  describe '#current_date' do
+    it 'returns the current date' do
+      expect(DateStamp.new.current_date).to eq Date.today.strftime('%d-%m-%Y')
+    end
+  end
+end
