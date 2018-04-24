71a126b653971586e6e278f035d5832c69f02954
Tom Betts <tom.betts@live.co.uk>
2018-04-24 17:01:50 +0100

    Refactor transaction_spec.rb with use of before(:each).

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -2,14 +2,16 @@ require 'transaction'
 
 describe Transaction do
   describe '#new' do
+    before(:each) do
+      @trn = Transaction.new('deposit', '10-05-2018')
+    end
+
     it 'is instantiated with a date' do
-      trn = Transaction.new('deposit', '10-05-2018')
-      expect(trn.date).to eq '10-05-2018'
+      expect(@trn.date).to eq '10-05-2018'
     end
 
     it 'is instantiated with a type' do
-      trn = Transaction.new('deposit', '10-05-2018')
-      expect(trn.type).to eq 'deposit'
+      expect(@trn.type).to eq 'deposit'
     end
   end
 end
